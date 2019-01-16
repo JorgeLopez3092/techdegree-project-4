c3c7095710205f41c08de4f9d922f78a674c1beb
JorgeLopez3092 <34284767+JorgeLopez3092@users.noreply.github.com>
2019-01-16 13:08:11 -0800

    Script finished
    Finished the script and modified some of the css to make sure it looks correct when the script starts hiding objects

--- js/search.js
+++ js/search.js
@@ -1,17 +1,22 @@
 function myFunction() {
   var myInput, filter, x, a, i;
   myInput = document.getElementById("search")
-  filter = input.value.toUpperCase();
-
-  x = document.getElementByTagName('a')
+  filter = myInput.value.toUpperCase();
+  x = document.getElementsByTagName('a')
   for(i = 0; i < x.length; i++) {
-    a = x[i].getElementsByAttribute('data-title')[0];
 
-    if(a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+
+    a = x[i].getAttribute('data-title');
+
+  if(a) {
+    if(a.toUpperCase().indexOf(filter) > -1) {
       x[i].style.display = "";
     } else {
       x[i].style.display = 'none';
     }
+  }
+
+
 
   }
 
